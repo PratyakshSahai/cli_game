b26617ab8a05086a2e6786d1e6a8451fdfc77dbd
Pratyaksh Sahai <pratyakshsahai@gmail.com>
2026-02-07 10:49:53 +0530

    Initial commit

---
+++ game.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Player {
+  int x, y;
+  bool hasKey;
+} player;
+
+void movePlayer();
+void loadMap();
+
+
+int score=0;
+
+int main() {
+
+
+
+  return 0;
+}
