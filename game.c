f532a3b8dd831551627a340ef00e76c223402ca9
Pratyaksh Sahai <pratyakshsahai@gmail.com>
2026-02-13 18:38:54 +0530

    fix: reset player position on map load and correct key collection logic

--- game.c
+++ game.c
@@ -11,25 +11,23 @@ typedef struct {
   char sprite;
 } Player;
 
-void loadMap();
+void loadMap(Player *player);
 void renderMap();
 void movePlayer(Player *player, char ch);
 void checkCollision(Player *player, int i, int j);
 
-int m=0;
+int m=0;  // index of map_list
 char map_list[10][10] = {"map1.map", "map2.map", "map3.map", "map4.map", "map5.map", "map6.map", "map7.map", "map8.map", "map9.map", "map10.map"};
 char map[13][21]={0};
 
 int main() {
   Player player;
 
-  player.x=1;
-  player.y=1;
   player.sprite='@';
   player.score=0;
   
   char ch = '\0';
-  loadMap();
+  loadMap(&player);
 
   while ((ch = tolower(getch())) != 'q') {
     movePlayer(&player, ch);
@@ -41,8 +39,7 @@ int main() {
 
 void renderMap() {
 
-  system("cls");
-
+  system("cls");  // clear screen
   printf("MAP %d\n", m+1);
 
   for (int i=0; i<13; i++) {
@@ -66,22 +63,23 @@ void checkCollision(Player *player, int i, int j) {
 
     if (map[*y][*x] == 'T')
       player->score++;  // treasure obtained
-
-    map[*y][*x]=player->sprite;
     
     if (map[*y][*x] == 'K') {
       m++;  // increment map number (goto next map)
-      loadMap();  // load next map
+      printf("%d", m);
+      loadMap(player);  // load next map
     }
+
+    map[*y][*x]=player->sprite;
   }
 
 }
 
 void movePlayer(Player *player, char ch) {
 
-  int i=0,j=0;
+  int i=0,j=0;  // i is CHANGE in y coordinate, j is CHANGE in x coordinate
   if (ch == 'w') {
-    i=-1,j=0;
+    i=-1,j=0; 
   } else if (ch == 'a') {
     i=0, j=-1;
   } else if (ch == 's') {
@@ -90,26 +88,26 @@ void movePlayer(Player *player, char ch) {
     i=0, j=1;
   }
 
-  checkCollision(player, i, j);  // i is CHANGE in y coordinate, j is CHANGE in x coordinate
+  checkCollision(player, i, j);
 }
 
-void loadMap() {
+void loadMap(Player *player) {
 
   system("cls");
+  player->x = player->y = 1;
 
   printf("MAP %d\n", m+1);
 
   char ch='\0';
-  FILE *file = fopen(map_list[m], "r");
+  FILE *file = fopen(map_list[m], "r"); // filename is map_list element
 
   int i=0, j=0;
   while ((ch = fgetc(file)) != EOF) {
     
-    if (ch == '\n') {
+    if (ch == '\n')
       continue;
-    } 
 
-    map[i][j]=ch;
+    map[i][j]=ch; // store each character read from file into 'map' matrix
     j++;
 
     if (j==21) {
@@ -119,6 +117,7 @@ void loadMap() {
 
   }
 
+  // printing the map
   for (int i=0; i<13; i++) {
     for (int j=0; j<21; j++) {
       printf("%c", map[i][j]);
@@ -129,3 +128,4 @@ void loadMap() {
 }
 
 // handle player symbol change
+// reset player position on new map load DONE
