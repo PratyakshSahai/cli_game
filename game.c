dd2b093b23f6507a11af2468a903a96bae8614c8
Shiwang Rao <shiwangrao20@gmail.com>
2026-04-05 16:05:54 +0530

    Added Random Maze generator

--- game.c
+++ game.c
@@ -2,7 +2,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <time.h>
 #include <conio.h>
+
+#define ROWS 6
+#define COLS 10
 
 #define WALL_BG "\x1b[47m  \x1b[0m"
 #define KEY "\x1b[41m  \x1b[0m"
@@ -16,13 +20,24 @@ typedef struct {
   char sprite;
 } Player;
 
+typedef struct {
+    int visited;
+    int walls[4];
+} Cell;
+
 void loadMap(Player *player);
 void renderMap(Player *player);
 int movePlayer(Player *player, char ch);
 int checkCollision(Player *player, int i, int j);
+void initMaze();
+void shuffle(int *arr, int n);
+void generateMaze(int x, int y);
+void buildGrid();
+void placeSpecialPoints();
+void saveMap(const char *filename);
+void generateMazeFile(const char *filename);
 
 int m=0;  // index of map_list
-char map_list[10][10] = {"map1.map", "map2.map", "map3.map", "map4.map", "map5.map", "map6.map", "map7.map", "map8.map", "map9.map", "map10.map"};
 char map[13][21]={0};
 
 int main() {
@@ -31,7 +46,7 @@ int main() {
   player.sprite='@';
   player.score=0;
   player.hasTreasure=false;
-  
+  generateMazeFile("maze.map");
   char ch = '\0';
   loadMap(&player);
 
@@ -130,7 +145,7 @@ int movePlayer(Player *player, char ch) {
 void loadMap(Player *player) {
 
   char ch='\0';
-  FILE *file = fopen(map_list[m], "r"); // filename is map_list element
+  FILE *file = fopen("maze.map", "r"); // filename is map_list element
 
   int i=0, j=0;
   while ((ch = fgetc(file)) != EOF) {
@@ -155,3 +170,178 @@ void loadMap(Player *player) {
 
 // handle player symbol change
 // reset player position on new map load DONE
+
+//Maze generator
+Cell maze[ROWS][COLS];
+char grid[ROWS * 2 + 1][COLS * 2 + 1];
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int sx, sy; // start
+int kx, ky; // key
+int ex, ey; // exit
+//Treasures
+int tx1, ty1;
+int tx2, ty2;
+int tx3, ty3;
+
+void initMaze() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            maze[i][j].visited = 0;
+            for (int k = 0; k < 4; k++)
+                maze[i][j].walls[k] = 1;
+        }
+    }
+}
+
+void shuffle(int *arr, int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
+
+void generateMaze(int x, int y) {
+    maze[x][y].visited = 1;
+
+    int dirs[4] = {0, 1, 2, 3};
+    shuffle(dirs, 4);
+
+    for (int i = 0; i < 4; i++) {
+        int d = dirs[i];
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+
+        if (nx >= 0 && nx < ROWS && ny >= 0 && ny < COLS && !maze[nx][ny].visited) {
+            maze[x][y].walls[d] = 0;
+            maze[nx][ny].walls[(d + 2) % 4] = 0;
+            generateMaze(nx, ny);
+        }
+    }
+}
+
+void buildGrid() {
+    int rows = ROWS * 2 + 1;
+    int cols = COLS * 2 + 1;
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            grid[i][j] = '#';
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            int gx = i * 2 + 1;
+            int gy = j * 2 + 1;
+
+            grid[gx][gy] = ' ';
+
+            if (!maze[i][j].walls[0]) grid[gx - 1][gy] = ' ';
+            if (!maze[i][j].walls[1]) grid[gx][gy + 1] = ' ';
+            if (!maze[i][j].walls[2]) grid[gx + 1][gy] = ' ';
+            if (!maze[i][j].walls[3]) grid[gx][gy - 1] = ' ';
+        }
+    }
+}
+
+// Place S, T, K on valid empty cells
+void placeSpecialPoints() {
+    int rows = ROWS * 2 + 1;
+    int cols = COLS * 2 + 1;
+
+    // Start fixed at top-left open cell
+    sx = 1;
+    sy = 1;
+
+    // Exit fixed at bottom-right open cell
+    ex = rows - 2;
+    ey = cols - 2;
+
+    // Place key randomly
+    int count = 0;
+    // Treasure 1
+while (1) {
+    int x = rand() % rows;
+    int y = rand() % cols;
+
+    if (grid[x][y] == ' ' &&
+        !(x == sx && y == sy) &&
+        !(x == ex && y == ey)) {
+        tx1 = x;
+        ty1 = y;
+        break;
+    }
+}
+
+// Treasure 2
+while (1) {
+    int x = rand() % rows;
+    int y = rand() % cols;
+
+    if (grid[x][y] == ' ' &&
+        !(x == sx && y == sy) &&
+        !(x == ex && y == ey) &&
+        !(x == tx1 && y == ty1)) {
+        tx2 = x;
+        ty2 = y;
+        break;
+    }
+}
+
+// Treasure 3
+while (1) {
+    int x = rand() % rows;
+    int y = rand() % cols;
+
+    if (grid[x][y] == ' ' &&
+        !(x == sx && y == sy) &&
+        !(x == ex && y == ey) &&
+        !(x == tx1 && y == ty1) &&
+        !(x == tx2 && y == ty2)) {
+        tx3 = x;
+        ty3 = y;
+        break;
+    }
+}
+}
+
+void saveMap(const char *filename) {
+    FILE *fp = fopen(filename, "w");
+    if (!fp) return;
+
+    int rows = ROWS * 2 + 1;
+    int cols = COLS * 2 + 1;
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+
+            if (i == sx && j == sy)
+              fputc('@', fp);
+            else if ((i == tx1 && j == ty1) ||
+                     (i == tx2 && j == ty2) ||
+                     (i == tx3 && j == ty3))
+                        fputc('T', fp);
+            else if (i == ex && j == ey)
+              fputc('K', fp);
+            else
+              fputc(grid[i][j], fp);
+            }
+        fputc('\n', fp);
+    }
+
+    fclose(fp);
+}
+
+void generateMazeFile(const char *filename) {
+    srand(time(NULL));
+
+    initMaze();
+    generateMaze(0, 0);
+    buildGrid();
+    placeSpecialPoints();
+    saveMap(filename);
+}
